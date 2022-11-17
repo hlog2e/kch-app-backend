1abe9e0047a0b0c7bf4aa8372dbc2bc8e82cd9f7
김홍록 | HongRok <dev@hongrok.net>
2022-11-18 00:03:32 +0900

    mongo connect 부분 일부  수정

--- models/index.js
+++ models/index.js
@@ -4,7 +4,12 @@ require("./user");
 require("./meal");
 require("./registerCode");
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  auth: { authSource: "admin" },
+  dbName: "kch-app",
+  user: process.env.MONGO_USERNAME,
+  pass: process.env.MONGO_PASSWORD,
+});
 
 const mongo = mongoose.connection;
 
