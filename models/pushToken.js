f962c748870bb8600be81ceb03288e2299ec597a
김홍록 | HongRok <dev@hongrok.net>
2023-01-23 21:17:31 +0900

    일부 DB 모델 컬렉션 이름 변경

--- models/pushToken.js
+++ models/pushToken.js
@@ -5,7 +5,7 @@ const pushTokenSchema = new mongoose.Schema(
     _id: { type: String },
     user_id: { type: String },
   },
-  { timestamps: true, versionKey: false, collection: "push-tokens" }
+  { timestamps: true, versionKey: false, collection: "push_tokens" }
 );
 
 module.exports = mongoose.model("PushToken", pushTokenSchema);
