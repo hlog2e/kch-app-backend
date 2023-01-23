a789d759b8966378d37cdffb20e9cc561f83a926
김홍록 | HongRok <dev@hongrok.net>
2023-01-23 21:33:27 +0900

    admin 라우터 index.js 수정

--- routers/admin/index.js
+++ routers/admin/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const noticeRouter = require("./notice");
+const pushNotiRouter = require("./push-noti");
 
 router.use("/notice", noticeRouter);
+router.use("/push-noti", pushNotiRouter);
 
 module.exports = router;
