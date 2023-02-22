8db47870f153d0e798341a2c4d9a0836e1a5cdbc
김홍록 | HongRok <dev@hongrok.net>
2023-02-22 21:15:07 +0900

    가입코드 카운트 제공해주는 로직과 사용, 미사용 구분하여 가입코드 제공해주는 로직 완성

--- controllers/admin/register-code.admin.controller.js
+++ controllers/admin/register-code.admin.controller.js
@@ -1,6 +1,25 @@
 const RegisterCode = require("../../models/registerCode");
 
 module.exports = {
+  getRegisterCodeInfo: async (req, res) => {
+    const allRegisterCodeCount = await RegisterCode.count({});
+    const unUsedRegisterCodeCount = await RegisterCode.count({ isUsed: false });
+    const usedRegisterCodeCount = await RegisterCode.count({ isUsed: true });
+
+    res.json({
+      all: allRegisterCodeCount,
+      unUsed: unUsedRegisterCodeCount,
+      used: usedRegisterCodeCount,
+    });
+  },
+  getRegisterCodes: async (req, res) => {
+    const { isUsed } = req.query;
+
+    const codes = await RegisterCode.find({ isUsed: isUsed });
+
+    res.json(codes);
+  },
+
   createRegisterCodeMany: async (req, res) => {
     const { amount } = req.body;
     let codesArray = [];
--- routers/admin/register-code.js
+++ routers/admin/register-code.js
@@ -4,8 +4,14 @@ const router = express.Router();
 const registerCodeController = require("../../controllers/admin/register-code.admin.controller");
 
 const { validator } = require("../../middlewares/exporess-validator");
-const { body } = require("express-validator");
+const { body, query } = require("express-validator");
 
+router.get("/info", registerCodeController.getRegisterCodeInfo);
+router.get(
+  "/",
+  [query("isUsed").notEmpty(), validator],
+  registerCodeController.getRegisterCodes
+);
 router.post(
   "/many",
   [body("amount").notEmpty(), validator],
