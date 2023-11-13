05ef01a6c277f53df23e8b9e1ae4ef3d352d4da0
김홍록 | HongRok <dev@hongrok.net>
2023-11-13 21:12:21 +0900

    actions test

--- app.js
+++ app.js
@@ -43,15 +43,7 @@ if (process.env.INSTANCE_VAR === "0") {
 }
 
 // CORS
-const whitelist = ["http://localhost:3000"];
 const corsOptions = {
-  //   origin: function (origin, callback) {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       callback(null, true);
-  //     } else {
-  //       callback(new Error("Not allowed by CORS"));
-  //     }
-  //   },
   origin: "*", //For All Allow
   credentials: true,
 };
